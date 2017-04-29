5fe08812caee3ef0872e1df0f76cdf5988fedb44
joelpalmer <joel.palmer@demand-energy.com>
2017-04-29 11:49:47 -0500

    changes to duplicate

--- src/main/scala/com/problems/Lists.scala
+++ src/main/scala/com/problems/Lists.scala
@@ -4,5 +4,7 @@ package com.problems
   * Created by joelpalmer on 4/29/17.
   */
 class Lists {
-  def duplicate[A](ls: List[A]): List[A] = ls flatMap { e => List(e, e) }
+  def duplicate[A](list: List[A]): List[A] = {
+    list.flatMap ( el => List(el, el) )
+  }
 }
--- src/main/scala/lists.sc
+++ src/main/scala/lists.sc
@@ -1,3 +1,3 @@
 import com.problems.Lists
 val toDup = new Lists
-toDup.duplicate(List("Liam", "Sebastian", "Michelle"))
+toDup.duplicate(List("Liam", "Sebastian", "Michelle", "Dusty"))
